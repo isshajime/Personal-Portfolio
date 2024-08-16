afdbad9805c3f9938aaa6688b224df06ab8a221a
Issey <issys123412@gmail.com>
2024-08-16 10:52:48 -0400

    Set up SQLAlchemy

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from datetime import datetime
 
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///daily_quote.db"
+db.init_app(app)
+
+class Quote(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    date: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    quote: Mapped[str] = mapped_column(String(500), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), unique=True)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def hello_world():
